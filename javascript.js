c2b0df51640628cd42f47d47033791a4cb30ca2c
Gabriel McAlister <gmcalister88@gmail.com>
2023-02-16 15:35:12 -0500

    Created trailing effect

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
+//import id's container and grid-container from html and assign them to javascript variables
 const container = document.querySelector('#container');
 const gridContainer = document.querySelector('#grid-container');
 
+//create div variable that creates an html div. The div will also have the class 'grid' attached
+// along with width, height, and background color adjustments.
 const div = document.createElement('div');
 div.classList.add('grid');
 div.style.width = '10px';
 div.style.height = '10px';
 div.style.backgroundColor = 'black';
 
+//create function that creates an adjustable grid out of the previously initiated div.
+//note that the cloneNode allows multiple copies of the div variable to be appended.
 function createGrid(){
     for( i = 0; i < 16; i++){
         for(j = 0; j < 16; j++){
@@ -14,13 +19,22 @@ function createGrid(){
         }
     }
 }
+//call upon the function to create the grid.
 createGrid();
 
-const grid = document.querySelector('.grid');
-grid.addEventListener('mouseover', function (e){
-    console.log(e);
-    e.target.style.backgroundColor = 'yellow';
+//create a grid variable that imports all
+const grid = document.querySelectorAll('.grid');
+grid.forEach((grid) => {
+    grid.addEventListener('mouseover', function (e){
+        console.log(e);
+        e.target.style.backgroundColor = 'yellow';
+    });
 });
+// div.addEventListener('mouseover', function (e){
+//     console.log(e);
+//     e.target.style.backgroundColor = 'yellow';
+//     grid.style.backgroundColor = 'yellow';
+// });
 
 
 
